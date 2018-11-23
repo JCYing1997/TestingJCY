7af3ab8ea6db6dc3ea58c1eb16974aeec58ef892
TARUC <TARUC@PC-104>
2018-11-23 10:49:16 +0800

    Program Ver 1.2

--- Testing/src/testing/Testing.java
+++ Testing/src/testing/Testing.java
@@ -16,6 +16,7 @@ public class Testing {
      */
     public static void main(String[] args) {
         System.out.println("HAHAHAHHAHAHAHAHA");
+        System.out.println("XDXDDXDXDXXD");
     }
     
 }
